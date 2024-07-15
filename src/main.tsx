9f28aa63214f5dece87f1255a0f4484ab7f7ad1e
Netson Cavina <netsoncavina@gmail.com>
2024-07-15 19:58:23 -0300

    import do theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
-
-const colors = {
-  primary: {
-    900: "#091428",
-  },
-  secondary: {
-    900: "#C89B3C",
-  },
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-const theme = extendTheme({ colors });
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

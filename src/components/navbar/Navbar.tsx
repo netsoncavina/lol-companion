1f4285736077178d043541ba756bd5939927056a
Netson Cavina <netsoncavina@gmail.com>
2024-07-15 20:43:43 -0300

    Refatoração da navegação

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,61 +1,234 @@
-import {
-  Flex,
-  Heading,
-  Avatar,
-  AvatarBadge,
-  AvatarGroup,
-} from "@chakra-ui/react";
-import SearchInput from "./SearchInput";
+'use client'
 
-const NavBar: React.FC = () => {
+import {
+  IconButton,
+  Avatar,
+  Box,
+  CloseButton,
+  Flex,
+  HStack,
+  VStack,
+  Icon,
+  useColorModeValue,
+  Text,
+  Drawer,
+  DrawerContent,
+  useDisclosure,
+  BoxProps,
+  FlexProps,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/react'
+import {
+  FiHome,
+  FiSettings,
+  FiMenu,
+  FiBell,
+  FiChevronDown,
+  FiUsers,
+  FiUser,
+  FiTv
+} from 'react-icons/fi'
+import { IconType } from 'react-icons'
+
+
+interface LinkItemProps {
+  name: string
+  icon: IconType
+}
+
+interface NavItemProps extends FlexProps {
+  icon: IconType
+  children: React.ReactNode
+}
+
+interface MobileProps extends FlexProps {
+  onOpen: () => void
+}
+
+interface SidebarProps extends BoxProps {
+  onClose: () => void
+}
+
+const LinkItems: Array<LinkItemProps> = [
+  { name: 'Home', icon: FiHome },
+  { name: 'Champions', icon: FiUser },
+  { name: 'Teams', icon: FiUsers },
+  { name: 'Match History', icon: FiTv },
+  { name: 'Settings', icon: FiSettings },
+]
+
+const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
+  return (
+    <Box
+      transition="3s ease"
+      bgGradient={"linear(to-r, blue_gradient.1, blue_gradient.2)"}
+      borderRight="2px"
+      borderRightColor="gold.2"
+      w={{ base: 'full', md: 60 }}
+      pos="fixed"
+      h="full"
+      {...rest}>
+      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold" 
+          color={"gold.2"}
+        >
+          League Companion
+        </Text>
+        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+      </Flex>
+      {LinkItems.map((link) => (
+        <NavItem key={link.name} icon={link.icon}>
+         {link.name}
+        </NavItem>
+      ))}
+    </Box>
+  )
+}
+
+const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
+  return (
+    <Box 
+      as="a"
+      href="#"
+      style={{ textDecoration: 'none', color: "gold.1" }}
+      _focus={{ boxShadow: 'none' }}>
+      <Flex
+        align="center"
+        p="4"
+        mx="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: 'gold.2',
+        }}
+        {...rest}>
+        {icon && (
+          <Icon
+            mr="4"
+            fontSize="24"
+            _groupHover={{
+              color: 'blue.5',
+            }}
+            color={"gold.2"}
+            as={icon}
+          />
+        )}
+        <Text
+          color={"gold.2"}
+          _groupHover={{
+            color: 'blue.5',
+          }}
+        >
+          {children}
+        </Text>
+      </Flex>
+    </Box>
+  )
+}
+
+
+const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
   return (
     <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      padding="1rem"
-      bg="primary.900"
-      color="secondary.900"
-    >
-      <>
-        <Heading as="h1" size="lg" color="secondary.900">
-          LOL Companion
-        </Heading>
-      </>
-      <Flex align="center" justify="center" wrap="wrap">
-        <Heading
-          fontSize="20px"
-          mr={8}
-          _hover={{
-            cursor: "pointer",
-          }}
-        >
-          Champions
-        </Heading>
-        <Heading
-          fontSize="20px"
-          mr={8}
-          _hover={{
-            cursor: "pointer",
-          }}
-        >
-          Players
-        </Heading>
-      </Flex>
-      <Flex>
-        <SearchInput />
-        <Avatar
-          size="sm"
-          name="Draven"
-          src="https://ddragon.leagueoflegends.com/cdn/14.2.1/img/champion/Draven.png"
-          _hover={{
-            cursor: "pointer",
-          }}
-        />
-      </Flex>
-    </Flex>
-  );
-};
+      ml={{ base: 0, md: 60 }}
+      px={{ base: 4, md: 4 }}
+      height="20"
+      alignItems="center"
+      bgGradient={"linear(to-r, blue_gradient.1, blue_gradient.2)"}
+      borderBottomWidth="2px"
+      borderBottomColor={"gold.2"}
+      justifyContent={{ base: 'space-between', md: 'flex-end' }}
+      {...rest}>
+      <IconButton
+        display={{ base: 'flex', md: 'none' }}
+        onClick={onOpen}
+        variant="outline"
+        aria-label="open menu"
+        icon={<FiMenu />}
+      />
 
-export default NavBar;
+      <Text
+        display={{ base: 'flex', md: 'none' }}
+        fontSize="2xl"
+        fontFamily="monospace"
+        fontWeight="bold"
+        color={"gold.2"}
+        >
+        League Companion
+      </Text>
+
+      <HStack spacing={{ base: '0', md: '6' }}>
+        <IconButton size="lg" variant="ghost" aria-label="open menu" icon={<FiBell />} />
+        <Flex alignItems={'center'}>
+          <Menu>
+            <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: 'none' }}>
+              <HStack>
+                <Avatar
+                  size={'sm'}
+                  src={
+                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                  }
+                />
+                <VStack
+                  display={{ base: 'none', md: 'flex' }}
+                  alignItems="flex-start"
+                  spacing="1px"
+                  ml="2">
+                  <Text fontSize="sm">Justina Clark</Text>
+                  <Text fontSize="xs" color="gray.600">
+                    Admin
+                  </Text>
+                </VStack>
+                <Box display={{ base: 'none', md: 'flex' }}>
+                  <FiChevronDown />
+                </Box>
+              </HStack>
+            </MenuButton>
+            <MenuList
+              bg={useColorModeValue('white', 'gray.900')}
+              borderColor="gold.1">
+              <MenuItem>Profile</MenuItem>
+              <MenuItem>Settings</MenuItem>
+              <MenuItem>Billing</MenuItem>
+              <MenuDivider />
+              <MenuItem>Sign out</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </HStack>
+    </Flex>
+  )
+}
+
+const SidebarWithHeader = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+      <SidebarContent onClose={() => onClose} display={{ base: 'none', md: 'block' }} />
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full">
+        <DrawerContent>
+          <SidebarContent onClose={onClose} />
+        </DrawerContent>
+      </Drawer>
+      {/* mobilenav */}
+      <MobileNav onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p="4">
+        {/* Content */}
+      </Box>
+    </Box>
+  )
+}
+
+export default SidebarWithHeader
